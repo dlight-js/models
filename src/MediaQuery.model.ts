30136358e8464871fb135c16bbb096856e72f57d
Gor <vosckaniangor@gmail.com>
2024-02-08 22:12:37 +0300

    `MediaQuery` model (#1)
    * feat: add `MediaQuery` model
    
    * feat: add `defaultState` prop

---
+++ src/MediaQuery.model.ts
@@ -0,0 +1,46 @@
+import {
+  type Modeling,
+  Model,
+  Pretty,
+  Prop,
+  Watch,
+  required,
+} from "@dlightjs/dlight"
+
+interface MediaQueryProps {
+  query: string
+  defaultState?: boolean
+}
+
+/**
+ * Represents a media query model.
+ * @example
+ *
+ * import { use } from "@dlightjs/dlight"
+ *
+ * mediaQuery = use(MediaQuery, { query: "(min-width: 768px)" })
+ * console.log(mediaQuery.matches) // Output: true or false depending on the media query
+ */
+@Model
+class MediaQuery {
+  @Prop query: MediaQueryProps["query"] = required
+  @Prop defaultState?: boolean
+
+  private mql = window.matchMedia(this.query)
+  public matches = this.defaultState ?? Boolean(this.mql.matches)
+
+  @Watch
+  watchQuery() {
+    this.mql.addEventListener("change", this.onChange)
+  }
+
+  willUnmount() {
+    this.mql.removeEventListener("change", this.onChange)
+  }
+
+  private onChange() {
+    this.matches = Boolean(this.mql.matches)
+  }
+}
+
+export default MediaQuery as Pretty as Modeling<MediaQuery, MediaQueryProps>
